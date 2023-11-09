7ef70465e91c29c7ed01e74a783bb9e2db53ef50
Gaiden92 <samy_fouchal@hotmail.fr>
2023-11-09 21:53:28 +0000

    creation User model

--- authentification/models.py
+++ authentification/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    CREATOR = "CREATOR"
+    SUSCRIBER = "SUSCRIBER"
+
+    ROLES_CHOICES = (
+    (CREATOR, "Createur"), 
+    (SUSCRIBER, "Abonné"),              
+   )                
+    profil_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLES_CHOICES)
+
